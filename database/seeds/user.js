3eb083e7eb562c6722ebe18d2b700d0b1b7c0d78
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-17 13:04:31 -0700

    ran migration and seed data need to build out helpers then i can run test

--- database/seeds/user.js
+++ database/seeds/user.js
@@ -1,13 +1,11 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("users").then(function() {
+    // Inserts seed entries
+    return knex("users").insert([
+      { name: "Bob the Builder", job: "Builder" },
+      { name: "John", job: "Computer Nerd" },
+      { name: "Mike", job: "Salesman" }
+    ]);
+  });
 };

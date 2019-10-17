41844fd2f13d8eb7cf5c7850451ea9818a1009f4
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-17 15:17:17 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const server = express();
 
 //Route Imports
-
+const userRoute = require("../api/routes/userRoute");
 server.use(express.json());
 
 //Routes
-
+server.use("/api/users", userRoute);
 //Deployment
 server.get("/", (req, res) => {
   res.status(200).json("Successful Deployment");

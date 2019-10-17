884a9491105819399c2ec42fbee0795ee533fa09
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-17 14:24:01 -0700

    Built endpoints now time for testing

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -15,7 +15,24 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  User.create(user);
+  User.create(user)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "failed to create new user", err });
+    });
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  id = req.params.id;
+  User.deleting(id)
+    .then(user => {
+      res.status(200).json({ message: "user deleted" });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "failed to create new user", err });
+    });
+});
+
+module.exports = router;
